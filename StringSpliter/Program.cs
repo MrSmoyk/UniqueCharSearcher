ffcbeac53d881626251c19d81d3e94c4e2716337
MrSmoyk <npankov96@gmail.com>
2023-02-05 18:10:10 +0200

    First commit

---
+++ StringSpliter/Program.cs
@@ -0,0 +1,17 @@
+﻿using StringSpliter;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        StringSplit splitter = new StringSplit();
+
+        var text = "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup";
+        var resp = splitter.UniqueChar(text);
+
+        Console.WriteLine(resp);
+
+        Console.ReadLine();
+        
+    }
+}
---
+++ StringSpliter/StringSplit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringSpliter
+{
+    public class StringSplit
+    {
+        public StringSplit()
+        {
+        }
+
+        public char? UniqueChar(string input)
+        {
+            string[] words = input.Split(" ");
+            var wordsList = new List<string>();
+
+            foreach (string word in words)
+            {
+                var w = Regex.Replace(word, "[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]", string.Empty);
+                
+                var ww = w.Replace(" ", "");
+
+                if (!string.IsNullOrEmpty(ww))
+                    wordsList.Add(ww);
+
+            }
+
+            string chars = string.Empty;
+
+            char? resp = '0';
+
+            foreach (string word in wordsList)
+            {
+                chars += word.GroupBy(c => c).Where(c => c.Count() == 1).FirstOrDefault()?.Key;
+            }
+
+            resp = chars.GroupBy(c => c).Where(c => c.Count() == 1).FirstOrDefault()?.Key;
+
+            return resp;
+
+        }
+    }
+}
