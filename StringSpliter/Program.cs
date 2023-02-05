f5cdc45385c2fd2c40fd4d7748c8e7ebbb7ed360
MrSmoyk <npankov96@gmail.com>
2023-02-05 18:12:01 +0200

    Second commit

--- StringSpliter/Program.cs
+++ StringSpliter/Program.cs
@@ -6,7 +6,7 @@ internal class Program
     {
         StringSplit splitter = new StringSplit();
 
-        var text = "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup";
+        var text = Console.ReadLine();
         var resp = splitter.UniqueChar(text);
 
         Console.WriteLine(resp);
